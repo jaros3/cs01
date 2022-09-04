803bd157551b8954a53114b77c5d245d3fd79325
KSolo0203 <jr.solovich@gmail.com>
2022-09-04 13:58:37 +0300

    After review

--- Task#4/Program.cs
+++ Task#4/Program.cs
@@ -3,8 +3,8 @@
 int FindMax (int arg1, int arg2, int arg3) // Решение с объявлением функции
 {
     int result = arg1;
-    if (arg2 > result) {result = arg2;}
-    else if (arg3 > result) {result = arg3;}
+    if (arg2 > result) result = arg2;
+    if (arg3 > result) result = arg3;
     return result;
 }
 
